bf76cf1546703ef180772339c43b24f2de0bffd9
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-05-25 19:54:52 -0400

    numpy_tutorial

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,7 @@
-print('data_processing.py')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+nyc_res = pd.read_csv('data/NYC_restaurants_orders/food_order.csv')
+nyc_res.info()
+
