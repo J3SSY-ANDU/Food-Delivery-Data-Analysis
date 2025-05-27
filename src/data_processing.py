6a011ac23a1433c8bb9c84f729a1c28eea944279
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-05-27 15:51:57 -0400

    organize project structure

--- src/data_processing.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-nyc_res = pd.read_csv('data/NYC_restaurants_orders/food_order.csv')
-food_delivery = pd.read_csv('data/food_delivery/test.csv')
-print(nyc_res.info())
-print(food_delivery.info())
-
